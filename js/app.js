8e9f0492993296e10dab36460d0583dbf512b8cc
vsheva <valera.sheva@gmail.com>
2024-05-20 23:27:17 -0400

    tracker statistic

--- js/app.js
+++ js/app.js
@@ -1,47 +1,90 @@
 class CalorieTracker {
-    constructor() {
-        this._calorieLimit = 3000;
-        this._totalCalories = 0;
-        this._meals = [];
-        this._workouts = [];
-    }
+  constructor() {
+    this._calorieLimit = 3300;
+    this._totalCalories = 0;
+    this._meals = [];
+    this._workouts = [];
 
-    addMeal(meal) {
-        this._meals.push(meal);
-        this._totalCalories+=meal.calories;
-    }
+    this._displayCaloriesLimit();
+    this._displayCaloriesTotal();
+    this._displayCaloriesConsumed();
+    this._displayCaloriesBurned();
+    this._displayCaloriesRemaining();
+  }
 
-    addWorkout(workouts) {
-        this._workouts.push(workouts);
-        this._totalCalories-=workouts.calories;
-    }
+  addMeal(meal) {
+    this._meals.push(meal);
+    this._totalCalories += meal.calories;
+    this._render();
+  }
+
+  addWorkout(workouts) {
+    this._workouts.push(workouts);
+    this._totalCalories -= workouts.calories;
+    this._render();
+  }
+
+  _displayCaloriesTotal() {
+    const totalCaloriesEl = document.getElementById('calories-total');
+    totalCaloriesEl.innerHTML = this._totalCalories;
+  }
+
+  _displayCaloriesLimit() {
+    const calorieLimitEl = document.getElementById('calories-limit');
+    calorieLimitEl.innerHTML = this._calorieLimit;
+  }
+
+  _displayCaloriesConsumed() {
+    const caloriesConsumed = document.getElementById('calories-consumed');
+
+    const consumed = this._meals.reduce((acc, meal) => acc + meal.calories, 0);
+    caloriesConsumed.innerHTML = consumed;
+  }
+
+  _displayCaloriesBurned() {
+    const caloriesBurnedEl = document.getElementById('calories-burned');
+
+    const burned = this._workouts.reduce((acc, workout) => acc + workout.calories, 0);
+    caloriesBurnedEl.innerHTML = burned;
+  }
+
+  _displayCaloriesRemaining() {
+    const caloriesRemainingEl = document.getElementById('calories-remaining');
+
+    const remaining = this._calorieLimit - this._totalCalories;
+    caloriesRemainingEl.innerHTML = remaining;
+  }
+
+  _render() {
+    this._displayCaloriesTotal();
+    this._displayCaloriesConsumed();
+    this._displayCaloriesBurned();
+    this._displayCaloriesRemaining();
+  }
 }
 
-
 class Meal {
-    constructor(name, calories) {
-        this.id = Math.random().toString(16).slice(2);
-        this.name=name;
-        this.calories= calories;
-    }
+  constructor(name, calories) {
+    this.id = Math.random().toString(16).slice(2);
+    this.name = name;
+    this.calories = calories;
+  }
 }
 
 class Workout {
-    constructor(name, calories) {
-        this.id = Math.random().toString(16).slice(2);
-        this.name=name;
-        this.calories= calories;
-    }
+  constructor(name, calories) {
+    this.id = Math.random().toString(16).slice(2);
+    this.name = name;
+    this.calories = calories;
+  }
 }
 
 const tracker = new CalorieTracker();
 
-const breakfast = new Meal("Breakfast", 400);
+const breakfast = new Meal('Breakfast', 400);
 tracker.addMeal(breakfast);
 
-const run = new Workout("Fast Run", 300);
+const run = new Workout('Fast Run', 330);
 tracker.addWorkout(run);
 
-console.log(tracker._meals , tracker._workouts, tracker._totalCalories );
-
-
+console.log(tracker._meals, tracker._workouts, tracker._totalCalories);
